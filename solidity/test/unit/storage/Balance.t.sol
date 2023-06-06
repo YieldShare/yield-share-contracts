4efb09a68892e84bbdee204da8f5c30c1f5b7f1a
agusduha <agusnduha@gmail.com>
2023-06-06 17:52:56 -0300

    fix: remove unused dependencies

--- solidity/test/unit/storage/Balance.t.sol
+++ solidity/test/unit/storage/Balance.t.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.17;
 
 import {DSTestFull} from 'test/utils/DSTestFull.sol';
-import {Balance} from '../../../contracts/storage/Balance.sol';
+import {Balance} from 'contracts/storage/Balance.sol';
 
 contract UnitBalance is DSTestFull {
   using Balance for Balance.Data;
